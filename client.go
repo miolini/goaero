11f965255e89b1838c7b4ab17be0921d01424796
Artem Andreenko <mio@volmy.com>
2014-07-08 19:00:31 +0400

    remove cgo_enabled directive

--- client.go
+++ client.go
@@ -24,7 +24,6 @@ package goaero
 
 // #cgo CFLAGS: -I./aerospike-client-c/target/Linux-x86_64/include
 // #cgo LDFLAGS: ./aerospike-client-c/target/Linux-x86_64/lib/libaerospike.a
-// #cgo CGO_ENABLED: 1
 // #include <aerospike/aerospike.h>
 // #include <aerospike/as_log.h>
 import "C"
